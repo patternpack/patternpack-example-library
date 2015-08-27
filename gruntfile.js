6fc8e45ac2b45f192d406dbd8268d5c7f0deac3e
John Gully <john@gully.us>
2015-08-26 20:52:03 -0500

    Added release variations to the example

--- gruntfile.js
+++ gruntfile.js
@@ -12,7 +12,10 @@ module.exports = function (grunt) {
       },
       run: {},
       build: {},
-      release: {}
+      release: {},
+      "release-major": {},
+      "release-minor": {},
+      "release-patch": {}
     }
   });
 
@@ -21,5 +24,8 @@ module.exports = function (grunt) {
   grunt.registerTask("run", ["patternpack:run"]);
   grunt.registerTask("build", ["patternpack:build"]);
   grunt.registerTask("release", ["patternpack:release"]);
+  grunt.registerTask("release-major", ["patternpack:release-major"]);
+  grunt.registerTask("release-minor", ["patternpack:release-minor"]);
+  grunt.registerTask("release-patch", ["patternpack:release-patch"]);
   grunt.registerTask("default", ["run"]);
 };
